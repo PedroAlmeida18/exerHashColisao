068905e3ca36bdfd7586e3e9ba3fd4125cc370ca
PedroAlmeida18 <109418292+PedroAlmeida18@users.noreply.github.com>
2023-04-23 00:09:50 +0000

    Mudanças e finalmente conseguir os tres testes

--- hashColisao/EstruturaHashTable.java
+++ hashColisao/EstruturaHashTable.java
@@ -1,62 +1,85 @@
 package hashColisao;
 
-public class EstruturaHashTable implements EstruturaDeDados{
+public class EstruturaHashTable implements EstruturaDeDados {
     private Integer tabela[];
+    private Integer tabelaextra[];
+    private int tamanho;
+
     //TODO adicionar elementos necessários para implementar o porão
 
     public EstruturaHashTable() {
-        int porão = 100;
-        int tamanho = 1000 + porão;
+        tabelaextra = new Integer[100];
         //esse número pode ser alterado
-        tabela = new Integer[tamanho+porão];
+        tabela = new Integer[1000];
+        tamanho = 100;
     }
 
     @Override
     public boolean insert(int chave) {
         int posição = calcularposi(chave);
-        for (int i = 0;i<tabela.length;i++){
-            if ( tabela[posição] == null || tabela[posição]== chave ){
-                tabela[posição] = chave ;
-                return true ;
-
+        if (tabela[posição] == null) {
+            tabela[posição] = chave;
+            return true;
+        } else {
+            int posiçãoex = calcularpoisextra(chave);
+            for (int i = 0; i < tamanho; i++) {
+                if (tabelaextra[(posiçãoex + i) % tamanho] == null) {
+                    tabelaextra[(posiçãoex + i) % tamanho] = chave;
+                    return true;
+                }
             }
-         
-           
         }
         return false;
-
-        // TODO quando inserir, se a posição estiver ocupada, 
-        //o elemento NÃO é inserido e retorna false. 
-        //Caso contrário, o elemento é inserido na posição calculada e retorna true.
-        
     }
+
+    // TODO quando inserir, se a posição estiver ocupada, 
+    //o elemento NÃO é inserido e retorna false. 
+    //Caso contrário, o elemento é inserido na posição calculada e retorna true.
 
     @Override
     public boolean delete(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, torna o elemento da posição como null e retorna true. Caso contrário, retorna false.
-        return false;
-        
+        int posição = calcularposi(chave);
+        if (tabela[posição] != null && tabela[posição] == chave) {
+            tabela[posição] = null;
+            return true;
+        } else {
+            int posiçãoextra = calcularpoisextra(chave);
+            for (int i = 0; i < tamanho; i++) {
+                if (tabelaextra[(posiçãoextra + i) % tamanho] != null
+                        && tabelaextra[(posiçãoextra + i) % tamanho] == chave) {
+                    tabelaextra[(posiçãoextra + i) % tamanho] = null;
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     @Override
     public int search(int chave) {
         int posição = calcularposi(chave);
-        for ( int i = 0 ; i< tabela.length;i++){
-            if( tabela[posição] == null){
-                return -1;
+        if (tabela[posição] != null && tabela[posição] == chave) {
+            return posição;
+        } else {
+            int posiçãoex = calcularpoisextra(chave);
+            for (int i = 0; i < tamanho; i++) {
+                if (tabelaextra[(posiçãoex + i) % tamanho] != null
+                        && tabelaextra[(posiçãoex + i) % tamanho] == chave) {
+                    return posiçãoex + i + 1000;
+                }
             }
-            else if (tabela[posição] == chave ){
-                return chave ;
-
-            }
+            return -1;
         }
-        // TODO se o elemento estiver presente retorna a sua posição. 
-        //Caso contrário, retorna -1.
-        return -1;
     }
-    public int calcularposi( int chave){
-        int posi= chave % 1100;
+
+    public int calcularposi(int chave) {
+        int posi = chave % 1000;
         return posi;
     }
 
+    public int calcularpoisextra(int chave) {
+        int posi = chave % 1000;
+        int posiçãoextra = posi % 100;
+        return posiçãoextra;
+    }
 }
